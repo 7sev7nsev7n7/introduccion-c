a75e217628bacae8634112feb190cb3126efabab
7sev7nsev7n7 <7sev7nsev7n7@gmail.com>
2025-04-05 17:47:21 -0600

    Added files for session 2, along with .gitignore to exclude .md source files

--- ejemplos/clase-01.c
+++ ejemplos/clase-01.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
 
+// Este ejemplo imprime texto
+// en la consola
 int main() {
   printf("Hola, Mundo!\n");
   printf("Este es mi primer programa hecho en C\n");
---
+++ ejemplos/clase-02.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main() {
+    // Establecemos una variable 'x' con valor 5
+    int x = 5;
+
+    // Establecemos una variable 'y' con valor 10
+    int y = 10;
+
+    // Sumamos los valores de 'x' y 'y', y guardamos
+    // dicho valor en la variable 'z'
+    int z = x+y;
+
+    // Imprimimos la suma de los dos valores
+    printf("La suma de 5 y 10 es %d\n", z);
+
+    // Devolvemos el valor 0 a la funcion main()
+    return 0;
+}
+
