bfdc35fc5996d78d3c0c6e75a105fb630f587e69
7sev7nsev7n7 <7sev7nsev7n7@gmail.com>
2025-04-13 15:23:36 -0600

    Removed example files, as to not clutter repository

--- ejemplos/clase-01.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-
-// Este ejemplo imprime texto
-// en la consola
-int main() {
-  printf("Hola, Mundo!\n");
-  printf("Este es mi primer programa hecho en C\n");
-  return 0;
-}
